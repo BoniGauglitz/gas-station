37f5175df72bb793ce15e604b4facbe9c33609f6
Bruno Gauglitz <bruno.gauglitz@gmail.com>
2015-12-11 17:49:28 -0200

    Format currency fields to float.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,17 @@
 class Station < ActiveRecord::Base
+  def gas_price=(value)
+    super(format_currency(value)) if value.present?
+  end
+
+  def etanol_price=(value)
+    super(format_currency(value)) if value.present?
+  end
+
+  def diesel_price=(value)
+    super(format_currency(value)) if value.present?
+  end
+
+  def format_currency(value)
+    value.gsub(/R\$/, '').gsub(',', '.').to_f
+  end
 end
